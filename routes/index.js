ca42f4b49cb8f18061e0276889b50e194db5088b
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-04-29 00:38:23 -0300

    ajustes index

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
-//const auth = require('../middlewares/auth');
+
 
 router.get('/', (req, res) => {
-   // console.log(res.locals.auth_data);
     console.log('GET INDEX');
-    return res.send({message: 'Bem-Vindo!'});
+    return res.status(200).send({ message: 'Bem-Vindo!' });
+
 });
 
 router.post('/', (req, res) => {
     console.log('POST INDEX');
     return res.send({message: 'Tudo ok com o método POST da raiz!'});
-    
 });
 
 module.exports = router;
