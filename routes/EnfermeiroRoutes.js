1e8ee880f1733e6229b991ef3f4b2286bea38313
Alessandra Freitas <alefreitas18@hotmail.com>
2020-05-07 20:56:18 -0300

    Atualizando

--- routes/EnfermeiroRoutes.js
+++ routes/EnfermeiroRoutes.js
@@ -35,7 +35,7 @@ router.get('/', (req, res) => {
 400 - Bad request
 401 - Unauthorized -- AUTENTICAÇÃO, tem caráter temporário.
 403 - Forbidden -- AUTORIZAÇÃO, tem caráter permanente.
-404 - Not found.
+404 - Not found..
 
 500 - Internal server error
 501 - Not implemented - a API não suporta essa funcionalidade
