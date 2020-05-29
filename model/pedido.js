6189bee26570d26ace5dfc4a8fd70e3f56cb9b91
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-05-28 22:24:49 -0300

    model pedido

--- model/pedido.js
+++ model/pedido.js
@@ -1,16 +1,26 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
-
-const PedidoSchema = new Schema({
-
-    idPedido: { type: String, required: true },
-    dataPedido: { type: Date, default: Date.now },
-    tipoServico: { type: String, required: true },
-    valorServico: { type: String, required: true },
+const schema = new Schema({
     cpfUsuario: { type: String, required: true },
+    emailUsuario: { type: String, required: true },
     cpfEnfermeiro: { type: String, required: true },
-    created: { type: Date, default: Date.now }
-});
+    emailEnfermeiro: { type: String, required: true },
+    tipoServico: { type: String, required: true },
+    enderecoServico: { type: String, required: true },
+    valorServico: { type: String, required: true },
+    status: { type: String },
 
+},{
+    timestamps: { createdAt: true, updatedAt: true },
+    toJSON: { 
+        virtuals: true,
+        transform(doc, ret) {
+            ret.id = ret._id
+            delete ret._id
+          }
+    },
+    versionKey: false,
+})
 
-module.exports = mongoose.model('Pedido', PedidoSchema);
+module.exports = mongoose.model('Pedido', schema);
